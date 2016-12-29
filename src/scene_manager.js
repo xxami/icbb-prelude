0ec64a91d6226acb57bccde594a7ca76ccea8b62
lilah <lilah.willacott@gmail.com>
2016-12-29 21:41:46 +0000

    add key input hooking code, implement basic title scene logic

--- src/scene_manager.js
+++ src/scene_manager.js
@@ -4,14 +4,32 @@ class SceneManager {
     constructor(containerId) {
         this._containerId = containerId;
         this._currentScene = null;
+        this._keyPressListeners = {};
+        this.addKeyPressEventListener();
     }
 
     get containerId() {
         return this._containerId;
     }
 
+    addKeyPressEventListener() {
+        document.addEventListener('keyup', function(event) {
+            if (event.keyCode in this._keyPressListeners) {
+                this._keyPressListeners[event.keyCode].apply(
+                    this._currentScene, event);
+            }
+        }.bind(this));
+    }
+
+    captureKeyPress(keyList, callback) {
+        keyList.forEach(function(key) {
+            this._keyPressListeners[key] = callback;
+        }.bind(this));
+    }
+
     switchScene(scene) {
         if (this._currentScene != null) {
+            this._keyPressListeners = {};
             this._currentScene.clear();
         }
         this._currentScene = scene;
--- src/title_scene.js
+++ src/title_scene.js
@@ -1,14 +1,37 @@
 
 var scene = require('./scene.js');
+var keys = require('./keys.js');
+var vnScene = require('./vn_scene.js');
 
 class TitleScene extends scene.Scene {
 
     init(sceneManager) {
         this.sceneManager = sceneManager;
+        this.container = document.getElementById(sceneManager.containerId);
         console.log('TitleScene::init');
+        this.playTitleBgm();
+        this.displayTitleGraphics();
+        this.sceneManager.captureKeyPress([keys.returnKey],
+            this.startGame);
+    }
+
+    playTitleBgm() {
+        soundManager.play('res/title_bgm', {loops: 3});
+    }
+
+    displayTitleGraphics() {
+        this.container.style.backgroundImage = 'url(res/title_bg.png)';
+    }
+
+    startGame() {
+        this.clear();
+        var scene = new vnScene.VisualNovelScene();
+        this.sceneManager.switchScene(scene);
     }
 
     clear() {
+        this.container.style.backgroundImage = '';
+        soundManager.stop('res/title_bgm');
         super.clear();
     }
 }
