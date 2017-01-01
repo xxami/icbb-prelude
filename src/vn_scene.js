3e217c545430f66a8e312b6b6032f576e5edd244
lilah <lilah.willacott@gmail.com>
2017-01-01 23:30:21 +0000

    add first draft visual novel engine; add boilerplate for scene end

--- src/vn_scene.js
+++ src/vn_scene.js
@@ -2,19 +2,68 @@
 var scene = require('./scene.js');
 var scenario = require('./vn_scenario.js');
 var keys = require('./keys.js');
+var orderScene = require('./order_scene.js');
 
 class VisualNovelScene extends scene.Scene {
 
     init(sceneManager) {
         this.sceneManager = sceneManager;
         this.container = document.getElementById(this.sceneManager.containerId);
-        let html = '<div id="container">'+
-        '<div id="textbox">text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here</div>'+
-        '</div>';
-        this.container.innerHTML = html;
         console.log('VisualNovelScene::init');
+        this.textBox = this.injectTextbox();
+        this.frameCount = 0;
+        this.frame = 0;
+        this.frameTexts = [];
+        this.setInitialTextState();
+        this.sceneManager.captureKeyPress([keys.returnKey],
+            this.progressNovel);
     }
 
+    progressNovel() {
+        this.sayText();
+    }
+
+    setInitialTextState() {
+        this.frameCount = scenario.frames.length;
+        this.renderFrame();
+    }
+
+    renderFrame() {
+        this.textBox.innerHTML = '';
+        let frame = scenario.frames[this.frame];
+        this.frameTexts = frame.say;
+        if ('bkg' in frame) {
+            console.log('VisualNovelScene::DrawBkg -> ', frame.bkg);
+        }
+        if ('sprites' in frame) {
+            console.log('VisualNovelScene::DrawSprites -> ', frame.sprites);
+        }
+        this.sayText();
+    }
+
+    sayText() {
+        if (this.frameTexts.length == 0) {
+            if (this.frame == (this.frameCount - 1)) {
+                this.clear();
+                var scene = new orderScene.OrderScene();
+                this.sceneManager.switchScene(scene);
+            }
+            else {
+                this.frame += 1;
+                this.renderFrame();
+            }
+        }
+        else {
+            let text = this.frameTexts.shift() + ' ';
+            this.textBox.innerHTML += text;
+        }
+    }
+
+    injectTextbox() {
+        let html = '<div id="container"><div id="textbox"></div></div>';
+        this.container.innerHTML = html;
+        return document.getElementById('textbox');
+    }
 
     clear() {
         super.clear();
